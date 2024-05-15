45e28962f25ae1ebed0c50e1755580061a1551b0
apaselina <kovpool@icloud.com>
2024-05-15 14:00:58 +0300

    create express app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const gamesRouter = require("./routes/games");
+const usersRouter = require("./routes/users");
+const categoriesRouter = require("./routes/categoties");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const PORT = 3000;
+app.use(
+  bodyParser.json,
+  express.static(path.join(__dirname, "public")),
+  usersRouter,
+  gamesRouter,
+  categoriesRouter
+);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
